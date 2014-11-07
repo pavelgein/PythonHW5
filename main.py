d7b3d72ea329cf1184c25a41bd437a4a39d468aa
Pavel Gein <pavelgejn@yandex.ru>
2014-11-07 13:23:10 +0500

    Add folder

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from game import Game
-from sys import argv
-from start_window import StartWindow
+from Catch.start_window import StartWindow
 import tkinter as tk
 
 
 
 def main():
-	# number_of_players = int(argv[1]) if len(argv) > 1 else 2
-	# game = Game(number_players = number_of_players)
-	main_window = StartWindow(master = tk.Tk())
+	master = tk.Tk()
+	master.title('Догони!')	
+	main_window = StartWindow(master = master)
 	main_window.start()
 
 if __name__ == '__main__':
